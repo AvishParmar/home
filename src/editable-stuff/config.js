8fc49ce83fe739c21631efde902a404ea73dcabc
AvishParmar <avishparmar53@gmail.com>
2022-09-17 22:19:02 -0400

    Updated resources

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,30 +6,26 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Avish",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Parmar",
+  message: " Student, Programmer, Researcher ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
-    },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
+      url: "https://github.com/AvishParmar",
     },
     {
       image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
+      url: "https://www.instagram.com/avishparmar/",
     },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/avish-parmar-40951915a/",
     },
     {
       image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
+      url: "https://twitter.com/AvpAvish",
     },
   ],
 };
@@ -51,11 +47,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/avishparmar.png"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "I am currently a Senior at Stony Brook University majoring in Computer Science with a specialization in Artificial Intelligence. I am passionate about using my technical skills and critical thinking to solve complex problems that benefit the world. In my free time I like to work on projects, practice & teach yoga, read books, and play drums.",
+  resume: require("../editable-stuff/AvishParmarResume2022.pdf"),
 };
 
 // PROJECTS SECTION
@@ -66,27 +62,27 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
-  reposLength: 4,
-  specificRepos: [],
+  gitHubUsername: "AvishParmar", //i.e."johnDoe12Gh"
+  reposLength: 0,
+  specificRepos: ["uamp", "Sentence-Representation-Models", "Word-Representation-Model", "Language-Model-Generation"],
 };
 
 // Leadership SECTION
 const leadership = {
-  show: false,
+  show: true,
   heading: "Leadership",
   message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+    "As a student I hold several leadership positions: Vice President of Stony Brook Computing Society, Peer Mentor for the College of Engineering and Applied Sciences, and a Teaching Assistant for Dept. of Computer Science. I am also an Undergraduate Research Assistant for Prof. Banerjee working on the Information Distortion Project; the goal of which is to create a ML model that detects distortions in news headlines and determines whether it is an exaggeration or a meosis. This model would then serve as a tool for individuals to detect fake news as well as determine the bias of their news provider.",
   images: [
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+      img: require("../assets/img/sbcs2.jpg"), 
+      label: "Vice President of SBCS", 
+      paragraph: "" 
     },
     { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+      img: require("../assets/img/ceas_peer_mentor.jpg"), 
+      label: "Peer Mentor", 
+      paragraph: "" 
     },
   ],
   imageSize: {
@@ -97,26 +93,26 @@ const leadership = {
 
 // SKILLS SECTION
 const skills = {
-  show: true,
+  show: false,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
+    { name: "Java", value: 90 },
+    { name: "Python", value: 85 },
     { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
+    { name: "Full Stack Development", value: 85 },
     { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "React", value: 85 },
+    { name: "HTML/CSS", value: 90},
+    { name: "MERN", value: 90 },
   ],
   softSkills: [
-    { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
+    { name: "Project Management", value: 92 },
+    { name: "Team-based work", value: 90 },
+    { name: "Critical Thinking", value: 90 },
     { name: "Adaptability", value: 85 },
-    { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
+    { name: "Problem Solving", value: 95 },
+    { name: "Approachability", value: 90 },
+    { name: "Organization", value: 86 },
     { name: "Creativity", value: 90 },
   ],
 };
@@ -126,30 +122,60 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for Software Engineering / Research Internship opportunities for Summer 2023, please feel free to contact me with my email",
+  email: "avishparmar53@gmail.com", 
 };
 
 const experiences = {
-  show: false,
+  show: true,
   heading: "Experiences",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      role: 'Developer Programs Engineer Intern',// Here Add Company Name
+      companylogo: require('../assets/img/Google-logo.png'),
+      date: 'May 2022 - August 2022',
     },
     {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      role: 'Undergraduate Research Assistant',// Here Add Company Name
+      companylogo: require('../assets/img/DeptOfCS3.jpg'),
+      date: 'September 2022 - Present',
+    },
+    {
+      role: 'Researcher/Full Stack Developer',// Here Add Company Name
+      companylogo: require('../assets/img/TBD2.png'),
+      date: 'January 2021 - Present',
+    },
+    {
+      role: 'Partner Relations',
+      companylogo: require('../assets/img/SBUHacks1.png'),
+      date: 'September 2022 - Present',
+    },
+    {
+      role: 'Fundamentals of Software Development (CSE316) Teaching Assistant',
+      companylogo: require('../assets/img/DeptOfCS3.jpg'),
+      date: 'August 2022 - Present',
+    },
+    {
+      role: 'Peer Mentor',
+      companylogo: require('../assets/img/CEAS.png'),
+      date: 'August 2021 - Present',
+    },
+    {
+      role: 'Event Coordinator',
+      companylogo: require('../assets/img/SBUHacks2.png'),
+      date: 'August 2021 - September 2022',
+    },
+    {
+      role: 'Data Structures & Algorithms (CSE214) Teaching Assistant',
+      companylogo: require('../assets/img/DeptOfCS3.jpg'),
+      date: 'August 2021 - December 2021',
     },
   ]
 }
 
 // Blog SECTION
-// const blog = {
-//   show: false,
-// };
+const blog = {
+  show: false,
+};
 
 export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
